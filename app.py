b4ef89a16f19329ac6e3ec5a70ff41991b99caaf
Isaiah Vickers <isaiah.vickers@outlook.com>
2023-09-20 17:02:25 -0400

    Count Files Organized Function

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+""" Clean The Desktop And Organize Files."""
 import os
 
 def check_if_file():
@@ -19,9 +20,36 @@ def get_file_extension(files_list):
     for file in files_list:
         file_extension = os.path.splitext(file)[1]
         extensions.add(file_extension)
-    return list(extensions)
+    return list(extensions) # returns one of each ext in list (Ex.".mp3")
+
+def count_files_organized(files):
+    """Returns the number of files moved."""
+
+    counted_files = len(files)
+    message = f"You have moved {counted_files} files."
+    print(message)
+
+    # Get exact files moved.
+    extension_counts = {}
+    for extension in files:
+        _, file_extension = os.path.splitext(extension)
+        ext_name = file_extension.lower() # removes the dot in extensions list
+
+        if ext_name in extension_counts:
+            extension_counts[ext_name] += 1
+        else:
+            extension_counts[ext_name] = 1
+
+    # Print the count of each extension
+    for extension, count in extension_counts.items():
+        if count == 1:
+            print(f"{count} {extension[1:]} file moved.")
+        else:
+            print(f"{count} {extension[1:]} files moved.")
+
 
 test = check_if_file()
 ext_test = get_file_extension(test)
+count_files_organized(test)
 
-print(ext_test)
+# print(count_test)
