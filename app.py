82a5e33147a0ce967dcfcd352f72c425f93e2f3e
Isaiah Vickers <isaiah.vickers@outlook.com>
2023-09-21 14:17:15 -0400

    Final Product

--- app.py
+++ app.py
@@ -8,12 +8,34 @@ from modules.file_extension import get_file_extension
 from modules.file_handle import count_files_organized, show_files_organized
 from modules.log_entry import create_log
 
-test = check_if_file()
-ext_test = get_file_extension(test)
+from os_functions.folder_manager import create_folders
+from os_functions.move_files import *
 
-files_organized = count_files_organized(test)
-files_moved = show_files_organized(test)
+# Today's Date
+today = datetime.datetime.now()
+current_day = today.strftime("%m_%d")
 
-print(files_moved, files_organized, test)
+files = check_if_file()
+extensions = get_file_extension(files)
 
-create_log(files_organized, files_moved, test)
+files_organized = count_files_organized(files)
+files_moved = show_files_organized(files)
+
+print(files_moved, files_organized, files)
+
+create_log(files_organized, files_moved, files)
+
+
+# Create Folders
+folder_path = "C:/Users/Isaiah Vickers/Desktop"
+folders = [
+    'Documents',
+    'Media Files',
+    'Audio Files',
+    'Zip Files',
+    'Setup Files',
+    'Files To Be Reviewed'
+]
+
+create_folders(folder_path, folders)
+move_all_files(folder_path, current_day)
